2a6c8a5e48b116ff08106a4aec8b8df4ef043984
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-24 23:30:49 +0530

    map

---
+++ 05_iterations/seven.js
@@ -0,0 +1,26 @@
+//Lec 30: Filter map and reduce in javascript(Part: 2)
+
+const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+/*
+const newNums = myNumers.map( (num) => num + 10)
+//or
+//const newNums = myNumers.map( (num) => { return num + 10})
+console.log(newNums);
+*/
+
+
+
+
+
+//chaining -> 2,3 method direct laga do
+//Q.pehle each nums 10 se multiply kr do then each nums mai 1 add kr do then print only the numbers that are greater than 40
+const newNums = myNumers
+                    .map((num) => num * 10)
+                    .map((num) => num + 1)
+                    .filter((num) => num >= 40)
+console.log(newNums);
+               
+
+
