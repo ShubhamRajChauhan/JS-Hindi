8d9c13acfdb62d9c7521cc0b6ef65742547db301
Shubham <shubhamrajchauhan07@gmail.com>
2024-12-25 00:15:56 +0530

    reduce

---
+++ 05_iterations/eight.js
@@ -0,0 +1,45 @@
+//Lec 30: Filter map and reduce in javascript(Part: 3)
+//-----------------Reduce--------------------------------
+
+const myNums = [1, 2, 3]
+
+
+/*
+const myTotal = myNums.reduce(function (acc, currval) {
+    console.log(`acc: ${acc} and currval: ${currval}`);
+    return acc + currval
+}, 0)
+
+console.log(myTotal);
+*/
+
+
+/*
+//write the same using arrow function
+const myTotal = myNums.reduce( (acc, curr) => acc+curr, 0)
+console.log(myTotal);
+*/
+
+
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 2999
+    },
+    {
+        itemName: "py course",
+        price: 999
+    },
+    {
+        itemName: "mobile dev course",
+        price: 5999
+    },
+    {
+        itemName: "data science course",
+        price: 12999
+    },
+]
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0)
+
+console.log(priceToPay);
