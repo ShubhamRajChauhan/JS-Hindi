bc6d8829e7afa003604e28238b08ca708bcc8aed
Shubham <shubhamrajchauhan07@gmail.com>
2025-01-29 03:44:55 +0530

    Getter Setter

---
+++ 10_classes_and_oop/getter_setter.js
@@ -0,0 +1,27 @@
+//Lec 48: Getter Setter and Stack Overflow
+
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()   //use _
+    }
+    set email(value){
+        this._email = value   //use _
+    }
+
+    get password(){
+        return `${this._password}hitesh`   //use _
+    }
+
+    set password(value){
+        this._password = value    //use _
+    }
+}
+
+const hitesh = new User("h@hitesh.ai", "abc")
+console.log(hitesh.password);  //abchitesh
+console.log(hitesh.email); //H@HITESH.AI
